4d58987c0910d7f0e502511850368920c610a929
Chloe <czeller@oxy.edu>
2016-04-22 17:59:58 -0700

    updated loop to convert search parameters to results page
    still need to update base.html to parse out that data and not make it ugly
    missing:
    
    * styling
    * visual
    
    otherwise, things are working!

---
+++ js/selection.js
@@ -0,0 +1,13 @@
+var year = document.getElementById("year_selection");
+alert( year.options[ year.selectedIndex ].value)
+
+var term = document.getElementById("term_selection");
+alert( term.options[ term.selectedIndex ].value )
+
+var core_recs = document.getElementById("core_recs_selection");
+alert( core_recs.options[ core_recs.selectedIndex ].value )
+
+var dept = document.getElementById( "dept_selection" );
+alert( dept.options[ dept.selectedIndex ].value )
+
+
--- my_course_counts.py
+++ my_course_counts.py
@@ -1,20 +1,21 @@
 from os import chdir
 from os.path import dirname, realpath
 
-from flask import Flask, render_template, send_from_directory
+from flask import Flask, render_template, send_from_directory, request
 
 app = Flask(__name__)
 
 # FIXME write your app below
 
 class Course_Info:
-    def __init__(self, year, season, department, course_num, section_title, units, professor, schedule, core,
+    def __init__(self, year, season, department, course_num, section, title, units, professor, schedule, core,
                  total_seats, enrolled, reserved, reserved_open, waitlisted):
         self.year = year
         self.season = season
         self.department = department
         self.course_num = course_num
-        self.section_title = section_title
+        self.section = section
+        self.title = title
         self.units = units
         self.professor = professor
         self.schedule = schedule
@@ -29,12 +30,13 @@ def get_data():
     course_data = []
     with open('counts.tsv') as file:
         for line in file.read().splitlines():
-            year, season, department, course_num, section_title, units, professor, schedule, core, total_seats, \
+            year, season, department, course_num, section, title, units, professor, schedule, core, total_seats, \
             enrolled, reserved, reserved_open, waitlisted = line.split('\t')
-            course_data.append(Course_Info(year, season, department, course_num, section_title, units, professor,
+            course_data.append(Course_Info(year, season, department, course_num, section, title, units, professor,
             schedule, core, total_seats, enrolled, reserved, reserved_open, waitlisted))
     return course_data
 
+# how do we know when we have that weird indexing issue that we have to deal with?
 
 
 # check the course_info class and just see if that's the right thing to do and then check the get_data function. I don't
@@ -52,19 +54,33 @@ Design, what should entire website looks like, what should CSS look like? What s
 def view_root():
     return render_template('base.html')
 
+
+
 @app.route('/results') # This function needs to have a loop that acquires the right information to be displayed on the
-'''
-Basic structure of what goes here:
-We might need a loop for each dropdown meun.
-new_list=[]
-For selection in dropdown:
-    take value of selection
-    for value in course_data,
-        new_list.append all the things at the indices surrounding the thing you selected
-If someone selected something from this menu, then, take the value
-associated with the thing they checked, and then loop through the course_info list. If the object in the course_info
-list matches the thing you clicked on the dropdown, then do basically what we did in the flask lab.
-'''
+# figure out the URL would be for the selections
+def view_course_info():
+    year = request.args.get('year')
+    term = request.args.get('term')
+    core_recs = request.args.get('core_recs')
+    dept = request.args.get('dept')
+    all_course_info = get_data()
+    length = len(all_course_info)
+    results = []
+    for index in range(length):
+        current_course = all_course_info[index]
+        match = True
+        if year is not None and current_course.year != year:
+            match = False
+        if term is not None and current_course.season != term:
+            match = False
+        if core_recs is not None and current_course.core != core_recs:
+            match = False
+        if dept is not None and current_course.department != dept:
+            match = False
+        if match:
+            results.append(current_course)
+    return render_template('filtered_page.html', list=results)
+
 
 def view_results():
     return render_template('filtered_page.html')
