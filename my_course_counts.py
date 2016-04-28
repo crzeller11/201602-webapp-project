5d9dedf45bd23b2e6d1cdb5c232b093e39f92b99
ronald1 <chan.jl.ronald@gmail.com>
2016-04-28 10:29:37 -0700

    HTML is streamlined
    Python is made shorter as I made changes to the get data and course_info

--- my_course_counts.py
+++ my_course_counts.py
@@ -1,47 +1,41 @@
+import csv
 from os import chdir
 from os.path import dirname, realpath
 
 from flask import Flask, render_template, send_from_directory, request
 
+TSV_FILE = 'counts.tsv'
+
 app = Flask(__name__)
 
-# FIXME write your app below
 
 class Course_Info:
-    def __init__(self, year, term, dept, course_num, section, title, units, professor, schedule, core_recs,
-                 total_seats, enrolled, reserved, reserved_open, waitlisted):
-        self.year = year
-        self.term = term
-        self.dept = dept
-        self.course_num = course_num
-        self.section = section
-        self.title = title
-        self.units = units
-        self.professor = professor
-        self.schedule = schedule
-        self.core_recs = core_recs
-        self.total_seats = total_seats
-        self.enrolled = enrolled
-        self.reserved = reserved
-        self.reserved_open = reserved_open
-        self.waitlisted = waitlisted
+    def __init__(self, row):
+        for k in row.keys():
+            setattr(self, k, row[k])
 
 
 def get_data():
     course_data = []
-    with open('counts.tsv') as file:
-        for line in file.read().splitlines():
-            year, term, dept, course_num, section, title, units, professor, schedule, core_recs, total_seats, \
-            enrolled, reserved, reserved_open, waitlisted = line.split('\t')
-            professor = professor.split(';')
-            professor = ", ".join(professor)
-            course_data.append(Course_Info(year, term, dept, course_num, section, title, units, professor,
-            schedule, core_recs, total_seats, enrolled, reserved, reserved_open, waitlisted))
+    with open(TSV_FILE, 'r') as tsvfile:
+        for row in csv.DictReader(tsvfile, delimiter='\t'):
+
+            row['instructors'] = row['instructors'].replace(';', ', ')
+
+            # Rename some keys
+            # PHASE THESE OUT
+            row['course_num'] = row.pop('number')
+            row['professor'] = row.pop('instructors')
+            row['schedule'] = row.pop('meetings')
+            row['core_recs'] = row.pop('core')
+            row['total_seats'] = row.pop('seats')
+
+            course_data.append(Course_Info(row))
     return course_data
+
 
 def get_prof_list():
     list_of_prof = []
-    course_data = get_data()
     for instance in course_data:
         if instance.professor not in list_of_prof:
             list_of_prof.append(instance.professor)
@@ -65,52 +59,32 @@ def view_root():
     return render_template('base.html', list_of_prof=list_of_prof)
 
 
-
 @app.route('/results')
 def view_course_info():
     year = request.args.get('year')
-    term = request.args.get('term')
+    season = request.args.get('term')
     core_recs = request.args.get('core_recs')
-    dept = request.args.get('dept')
+    department = request.args.get('dept')
     professor = request.args.get('professor')
-    all_course_info = get_data()
-    refined_year = []
-    refined_term = []
-    refined_core_recs = []
-    refined_dept = []
-    results = []
-    if year == "Please Select a Year":
-        refined_year = all_course_info
-    if year is not None:
-        for instance in all_course_info:
-            if instance.year == year:
-                refined_year.append(instance)
-    if term == "Please Select a Term":
-        refined_term = refined_year
-    if term is not None:
-        for instance in refined_year:
-            if instance.term == term:
-                refined_term.append(instance)
-    if core_recs == "Please Select Core":
-        refined_core_recs = refined_term
-    if core_recs is not None:
-        for instance in refined_term:
-            if instance.core_recs.find(core_recs) != -1:
-                refined_core_recs.append(instance)
-    if dept == "Please Select a Department":
-        refined_dept = refined_core_recs
-    if dept is not None:
-        for instance in refined_core_recs:
-            if instance.dept == dept:
-                refined_dept.append(instance)
-    if professor == "Please Select a Professor":
-        results = refined_dept
-    if professor is not None:
-        for instance in refined_dept:
-            if instance.professor.find(professor) != -1:
-                results.append(instance)
-    return render_template('filtered_page.html', results=results)
 
+    results = course_data
+
+    if year != "Please Select a Year":
+        results = [course for course in results if course.year == year]
+
+    if season != "Please Select a Term":
+        results = [course for course in results if course.season == season]
+
+    if core_recs != "Please Select Core":
+        results = [course for course in results if core_recs in course.core_recs]
+
+    if department != "Please Select a Department":
+        results = [course for course in results if course.department == department]
+
+    if professor != "Please Select a Professor":
+        results = [course for course in results if professor in course.professor]
+
+    return render_template('filtered_page.html', results=results)
 
 
 def view_results():
@@ -120,18 +94,23 @@ def view_results():
 # The functions below lets you access files in the css, js, and images folders.
 # You should not change them unless you know what you are doing.
 
+
 @app.route('/images/<file>')
 def get_image(file):
     return send_from_directory('images', file)
+
 
 @app.route('/css/<file>')
 def get_css(file):
     return send_from_directory('css', file)
 
+
 @app.route('/js/<file>')
 def get_js(file):
     return send_from_directory('js', file)
 
+
 if __name__ == '__main__':
     chdir(dirname(realpath(__file__)))
+    course_data = get_data()
     app.run(debug=True)
